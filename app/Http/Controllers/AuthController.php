af730df37570bbf97a36bfb8b9f42be72d630fc7
nrshagor <noorerabbishagor@gmail.com>
2024-08-27 17:35:16 +0600

    user login register and logout using sanctum token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,20 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        return 'register';
+        $fields =  $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed',
+        ]);
+        $user = User::create($fields);
+        $token = $user->createToken($request->name);
+        return ['user' => $user, 'token' => $token->plainTextToken];
     }
+
     public function login(Request $request)
     {
-        return 'login';
+        $request->validate([
+            'email' => 'required|email|exists:users',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return [
+                'message' => 'The provided credentials are incorrect'
+            ];
+        }
+        $token = $user->createToken($user->name);
+        return ['user' => $user, 'token' => $token->plainTextToken];
     }
     public function logout(Request $request)
     {
-        return 'logout';
+        $request->user()->tokens()->delete();
+        return [
+            'message' => 'You are Logged Out'
+        ];
     }
 }
