b4e3825f159e2296f0afa8c24523b2cc1e6b9bc4
nrshagor <noorerabbishagor@gmail.com>
2024-08-29 00:05:52 +0600

    unit test and feature test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,18 +2,39 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+    public function test_post_creation()
+    {
+        $user = User::factory()->create();
+
+        Sanctum::actingAs($user);
+
+        $postData = [
+            'title' => 'Sample Title',
+            'body' => 'Sample content of the post.',
+        ];
+
+        $response = $this->postJson('/api/posts', $postData);
+
+        $response->assertStatus(201)
+            ->assertJson(['data' => ['title' => 'Sample Title']]);
+
+        $this->assertDatabaseHas('posts', ['title' => 'Sample Title']);
+    }
     /**
      * A basic test example.
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        $blogPostApi = $this->get('api/posts');
 
-        $response->assertStatus(200);
+        $blogPostApi->assertStatus(200);
     }
 }
